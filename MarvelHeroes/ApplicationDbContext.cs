99f19b588b6e2eeac7d9ae4152e2fe16fb6739d0
thackerdynasty <70179471+thackerdynasty@users.noreply.github.com>
2025-08-15 20:02:16 -0500

    add db and controller

---
+++ MarvelHeroes/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MarvelHeroes;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Hero> Heroes { get; set; }
+}
--- MarvelHeroes/Program.cs
+++ MarvelHeroes/Program.cs
@@ -1,6 +1,10 @@
+using MarvelHeroes;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite("Data Source=MarvelHeroes.db"));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
